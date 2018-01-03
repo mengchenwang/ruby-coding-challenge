f4d56106935cac81e7c19006d9fad63566f4995c
mengchenwang <mengchenwang@hotmail.com>
2018-01-03 22:32:02 +0000

    q6 solved

--- lib/questions.rb
+++ lib/questions.rb
@@ -21,19 +21,26 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
-  
+  # invert is not safe when values are not unique
+  array = []
+  hash.each { |key, value| array << [value, key] }
+  array.to_h
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  hash.flatten.sum
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  hash = {}
+  array.each_slice(2) { |odd, even| hash.store(odd, even) }
+  hash
 end
 
 
