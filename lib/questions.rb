da21a264775c176c956cbb012f1079edd99149b6
mengchenwang <mengchenwang@hotmail.com>
2018-01-05 11:15:14 +0000

    q9 solved

--- lib/questions.rb
+++ lib/questions.rb
@@ -47,6 +47,7 @@ end
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+  string.gsub(/[[:upper:]]/, "")
 end
 
 
@@ -54,10 +55,12 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string.chomp(string[-(string.length/2)..-1])
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 
